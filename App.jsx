14fc5bfa686a755bc9c913521765e093d442be52
Joydeb Roy <68282805+his-helloworld@users.noreply.github.com>
2024-05-06 22:43:42 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,11 +1,42 @@
-import Chai from "./Chai";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
 function App() {
-  const username = "Joydeb Roy"
+  let [counter, setCounter] = useState(5);
+  let [rmCounter, setrmCounter] = useState(5);
+  // let counter = 5;
+  const addValue = () => {
+    if(counter >= 20) {
+      return;
+    }
+    // counter = counter + 1;
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    if (rmCounter === 0) {
+      return;
+    }
+    // counter = counter - 1;
+    setrmCounter(rmCounter - 1);
+  }
+
+  const handleClick = () => {
+    window.location.reload();
+  };
   return (
     <>
-    <Chai/>
-    <h2>Chai aur react with {username}</h2>
-    <p>test para</p>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}>Add value {counter}</button>
+      <br />
+      <button onClick={removeValue}>remove value {rmCounter}</button>
+      <br />
+      <button onClick={handleClick}>Refresh Page</button>
     </>
   )
 }

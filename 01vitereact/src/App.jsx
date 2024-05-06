20a5e5b5aaaf6a9b5f33edb68801208eb52e1e62
Joydeb Roy <joydebroy112000@gmail.com>
2024-05-06 22:36:27 +0530

    reactCounter

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./Chai";
+function App() {
+  const username = "Joydeb Roy"
+  return (
+    <>
+    <Chai/>
+    <h2>Chai aur react with {username}</h2>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
